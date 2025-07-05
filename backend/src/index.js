d36735cdd4e1b47c0fe0e91e15b5f7565f76cffc
HarshvardhanR <rharsh989878@gmail.com>
2025-07-05 03:39:22 -0400

    added user schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
-const app = express();
+import {connectDB} from '../src/lib/db.js'
 
+const app = express();
+app.use(express.json());
+dotenv.config();
 app.use("/auth/api", authRoutes);
-app.listen(5001, ()=>{
-    console.log("Server listening on port 5001");
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+    console.log("Server listening on PORT: "+ PORT);
+    connectDB(); 
 })
