376acbd8dc42a897c35a14f5a19dad9223442fa5
HarshvardhanR <rharsh989878@gmail.com>
2025-07-06 02:58:45 -0400

    added profilePic update route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/update-profile", protectRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
